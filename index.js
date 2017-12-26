e00f4920195e8072b7d179f8f1a12fa83b335c95
erlendcognite <erlend.vollset@cognite.com>
2017-12-26 05:34:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/fe/styles", express.static(__dirname));
-app.use("/fe/images", express.static(__dirname + '/fe/images'));
-app.use("/fe/scripts", express.static(__dirname + '/fe/scripts'));
+//app.use("/fe/styles", express.static(__dirname));
+//app.use("/fe/images", express.static(__dirname + '/fe/images'));
+//app.use("/fe/scripts", express.static(__dirname + '/fe/scripts'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + 'fe/views/index.html'));
+  res.sendFile(path.join(__dirname + 'fe/index.html'));
 });
 
-// add other routes below
-app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname + 'fe/views/about.html'));
-});
+//// add other routes below
+//app.get('/about', function (req, res) {
+//  res.sendFile(path.join(__dirname + 'fe/views/about.html'));
+//});
 
 app.listen(process.env.PORT || 8080);
