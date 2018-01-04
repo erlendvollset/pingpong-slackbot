9ebacfc4f5a6e00481c4c7f4e4ff49a7d06abecc
erlendcognite <erlend.vollset@cognite.com>
2018-01-04 01:40:59 +0100

    Remove unused code from server.py

--- server.py
+++ server.py
@@ -1,28 +1,11 @@
 from flask import Flask
-from flask import send_from_directory
 from flask_cors import CORS, cross_origin
 import pingpong.db_services as db_services
 import json
 
-app = Flask(__name__, static_url_path="/frontend/dist")
+app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-@app.route('/')
-def root():
-    return send_from_directory("/frontend/dist", "index.html")
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory("/frontend/dist", path)
-
-@app.route('/static/js/<path:path>')
-def serve_static_js(path):
-    return send_from_directory("frontend/dist/static/js", path)
-
-@app.route('/static/css/<path:path>')
-def serve_static_css(path):
-    return send_from_directory("frontend/dist/static/css", path)
 
 @app.route("/hello", methods=['GET'])
 @cross_origin()
