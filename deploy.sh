bac0f6899eff070c4c3d51426af75be88176b1a9
erlendvollset <erlendvollset@gmail.com>
2021-10-27 19:53:33 +0200

    Refine deploy script and bump versoin

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,28 @@ set -e
 manifest_file="manifest.yaml"
 image_without_tag="eu.gcr.io/cognitedata-development/pingpong-slackbot"
 pattern=".*$image_without_tag:([0-9]+).*"
-if [[ $(<$manifest_file) =~ $pattern ]] 
+
+if ! [[ $(<$manifest_file) =~ $pattern ]]
 then
-	tag="${BASH_REMATCH[1]}"
-  new_tag=$(( tag + 1 ))
-	old_image="$image_without_tag:$tag"
-	new_image="$image_without_tag:$new_tag"
-else
-	echo "could find tag to replace in manifest"
-	exit 1
+  echo "could find tag to replace in manifest"
+  exit 1
 fi
 
-echo "old image: $old_image"
-echo "new image: $new_image"
+tag="${BASH_REMATCH[1]}"
+new_tag=$(( tag + 1 ))
+old_image="$image_without_tag:$tag"
+new_image="$image_without_tag:$new_tag"
 
-docker build . -t $new_image
-CLOUDSDK_PYTHON=/usr/bin/python3 docker push $new_image
-
-echo replacing image in manifest
+echo -e "\n*** Replacing image tag in manifest"
+echo "old: $old_image"
+echo "new: $new_image"
 sed -i "" "s+$old_image+$new_image+" $manifest_file
 
+echo -e "\n*** Building docker image"
+docker build . -t $new_image
+
+echo -e "\n*** Pushing docker image"
+CLOUDSDK_PYTHON=/usr/bin/python3 docker push $new_image
+
+echo -e "\n*** Applying manifest in cognitedata-development"
 kubectl --context dev apply -f $manifest_file
